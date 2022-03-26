115a7d7914e681e5fd0e408cc667c60b330756a7
cosmobowt <anishsp21@gmail.com>
2022-03-26 23:15:02 +0530

    commit

---
+++ Introductory Problems/Number Spiral/index.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fu(i, n) for (ll i = 0; i < n; i++)
+#define MOD 1000000007
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> P;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<ll> y(n), x(n);
+    fu(i, n)
+    {
+        cin >> y[i];
+        cin >> x[i];
+    }
+    fu(i, n)
+    {
+        if (x[i] > y[i])
+        {
+            if (x[i] % 2 == 1)
+            {
+                cout << ((x[i] * x[i]) - y[i] + 1) << endl;
+            }
+            else
+            {
+                x[i]--;
+                cout << ((x[i] * x[i]) + y[i]) << endl;
+            }
+        }
+        else
+        {
+            if (x[i] % 2 == 0)
+            {
+                cout << ((y[i] * y[i]) - x[i] + 1) << endl;
+            }
+            else
+            {
+                cout << ((y[i] * y[i]) - y[i] + 1 - (y[i] - x[i])) << endl;
+            }
+        }
+    }
+}
---
+++ Sorting and Searching/Ferris Wheel/index.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fu(i, n) for (int i = 0; i < n; i++)
+#define MOD 1000000007
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> P;
+
+int main()
+{
+    ll n, x, count = 0, i = 0;
+    cin >> n >> x;
+    vector<int> weight(n);
+    sort(weight.begin(), weight.end());
+    while (i < n)
+    {
+        if (weight)
+    }
+}
